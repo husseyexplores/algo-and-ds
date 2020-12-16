e21e1cc7ed307b5228c7a56669c56f422c473161
husseyexplores <husseyexplores@gmail.com>
2020-12-16 13:03:37 +0500

    Add canSum tabluation

--- algorithms/canSum.spec.js
+++ algorithms/canSum.spec.js
@@ -15,6 +15,8 @@ arguments:
 -------------------------------------------------------------------------*/
 
 const hasOwnProp = Object.prototype.hasOwnProperty
+
+// Recursive
 const canSum = (targetSum, numbers, memo = {}) => {
   if (hasOwnProp.call(memo, targetSum)) return memo[targetSum]
 
@@ -35,6 +37,28 @@ const canSum = (targetSum, numbers, memo = {}) => {
   return false
 }
 
+// Tabulation
+const canSumTab = (targetSum, numbers) => {
+  const table = Array(targetSum + 1).fill(false)
+  table[0] = true
+
+  const tableLen = table.length
+  const numbersLen = numbers.length
+
+  for (let i = 0; i < tableLen; i++) {
+    if (table[i] === false) continue
+
+    for (let j = 0; j < numbersLen; j++) {
+      const num = numbers[j]
+      if (i + num < tableLen) {
+        table[i + num] = true
+      }
+    }
+  }
+
+  return table[targetSum]
+}
+
 // -----------------------------------------------------------------------
 
 describe('Can Sum', () => {
@@ -43,9 +67,14 @@ describe('Can Sum', () => {
     expect(canSum(7, [5, 3, 4, 7])).toEqual(true)
     expect(canSum(7, [2, 4])).toEqual(false)
     expect(canSum(8, [2, 3, 5])).toEqual(true)
+    expect(canSumTab(7, [2, 3])).toEqual(true)
+    expect(canSumTab(7, [5, 3, 4, 7])).toEqual(true)
+    expect(canSumTab(7, [2, 4])).toEqual(false)
+    expect(canSumTab(8, [2, 3, 5])).toEqual(true)
   })
   it('Huge', () => {
     expect(canSum(300, [7, 14])).toEqual(false)
+    expect(canSumTab(300, [7, 14])).toEqual(false)
   })
 })
 
